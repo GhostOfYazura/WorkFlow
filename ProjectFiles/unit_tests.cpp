ca00fa0b5e4b90ea0437f527603f4aab96a52413
GhostOfYazura <GhostOfYazura@gmail.com>
2023-06-11 21:46:38 +0800

    - Added skeleton functions to unit_tests.cpp

--- ProjectFiles/unit_tests.cpp
+++ ProjectFiles/unit_tests.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <chrono>
+#include <fstream>
 
 class tests {
+    
+    // Redumentry timing test to check that functionality works on machine.
+    void timingTest() {
+        for (int i = 0; i < 1000; i++)
+        {
+            auto& startTime = std::chrono::steady_clock::now();        
+            auto& endTime = std::chrono::steady_clock::now();
+            std::chrono::microseconds runTime 
+                = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+            std::ofstream timingTestStream("timingTestPerformanceLog.txt", std::ios::in);
+            timingTestStream << runTime.count() << '\n'; 
+        }
+    }
+
     // Outputs the GPIO pin based on a random number using rand().
     void lightTest(){
-        std::cout << "COUT Works, called for lightTest worked. Linked correctly.\n";
+
     }
+
+    // Sets a random time between 0,10 seconds of motor spinning.
+    void motorTest(){
+        
+    }
+
+
 
 };
